f04b5efc5e584f579ff49e34cb518734290f8d55
ISeeTheFuture <leesh4719@gmail.com>
2020-05-11 12:49:45 +0900

    20200511 이분탐색 보완

--- src/baekjoon/biSearch/bj2869/Main.java
+++ src/baekjoon/biSearch/bj2869/Main.java
@@ -10,10 +10,19 @@ public class Main {
 		int b = sc.nextInt();
 		int v = sc.nextInt();
 		
-		int left = 0;
-		int right = v;
-		int mid = v;
-		int ans = v;
+		// 주의할 점 : left, right, mid, ans 는 전부 날짜. 거리 개념이 아님.
+				// 채점하면 이게 맞다고 하는데, 논리적으로는 오히려 틀림.
+				int left = 0;
+				int right = v;
+				int mid = v;
+				int ans = v;
+		
+				// 밑에처럼 나올 수 있는 최대 날짜 값을 입력하는 것이 맞음.
+					// 문제에 주어진 날짜 최대값이 없으므로, 이분탐색으로써는 문제 자체가 잘못되었다고 볼 수 있음.
+		//		int left = 0;
+		//		int right = 2000000000;
+		//		int mid = 2000000000;
+		//		int ans = 2000000000;
 		
 		int n = 1;
 		while(left < right) {
